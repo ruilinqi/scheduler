9a3ec4c0da22c115edf1b443885e4ed1f86b4f2f
ruilinqi <ruilinqi7@gmail.com>
2023-01-11 21:15:12 -0500

    Created a component Appointment

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Appointment from "components/Appointment/index.js";
+
+import "styles.scss";
+
+<article className="appointment"></article>
+
+storiesOf("Appointment", module)
+  // The function addParameters can be chained to storiesOf and can be passed an object of parameters.
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  // Add stories by chaining an function for any number of stories that we want to write
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
