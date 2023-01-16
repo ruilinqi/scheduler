211676b0889eaa5addf8b33b9b1f50c4b24207e4
ruilinqi <ruilinqi7@gmail.com>
2023-01-16 00:43:19 -0500

    Added helpers files

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = {
   "1": {
@@ -63,7 +64,22 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday"); // Set the default day state to "Monday"
-  
+  const [days, setDays] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+
+  useEffect(() => {
+    
+    Promise.all([
+      axios.get('/api/days'),
+      
+    ])
+}, []);
+
   // 
   const appointmentArray = Object.values(appointments).map((appointment) => {
     return (
---
+++ src/helpers/selectors.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+}
